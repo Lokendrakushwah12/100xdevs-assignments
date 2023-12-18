d4b1b116c5420af56da54a9f7de6cc3641e491db
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-12-18 20:53:13 +0530

    solved 01-js/easy/anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,39 @@
 */
 
 function isAnagram(str1, str2) {
-
+  // uppercase
+  str1=str1.toLowerCase()
+  str2=str2.toLowerCase()
+  
+  let str1Arr = str1.split("");
+  let str2Arr = str2.split("");
+  if(str1 == str2 || (str1=="" && str2=="") ) return true
+  if(str1.length!=str2.length) return false
+  let str1Obj = {};
+  let str2Obj = {};
+  let result = true;
+  for (let i = 0; i < str1Arr.length; i++) {
+    if (str1Obj[str1Arr[i]]) {
+      str1Obj[str1Arr[i]]++;
+    } else {
+      str1Obj[str1Arr[i]] = 1; 
+    }
+  }
+  for (let i = 0; i < str2Arr.length; i++) {
+    if (str2Obj[str2Arr[i]]) {
+      str2Obj[str2Arr[i]]++;
+    } else {
+      str2Obj[str2Arr[i]] = 1;
+    }
+  }
+  for (let key in str1Obj) { 
+    if (str1Obj[key] !== str2Obj[key]) {
+      result = false;
+    }
+  }
+  return result;
 }
+
+// console.log(isAnagram("Listen", "silent"));
 
 module.exports = isAnagram;
