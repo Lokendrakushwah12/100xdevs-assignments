54f51767bd53379e1139a20ba6f79970359e43f1
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-12-18 23:18:47 +0530

    solved: ./01-js except calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,41 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num == 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(str) {
+    
+  }
+}
 
 module.exports = Calculator;
--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,17 @@
 */
 
 function isPalindrome(str) {
+  str = str.replace(/[^\w]/g, '');
+  str = str.toLowerCase();
+  let i = 0;
+  let j = str.length - 1;
+  while (i < j) {
+    if (str[i] != str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
   return true;
 }
 
